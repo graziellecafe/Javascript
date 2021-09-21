5bc0517b88557e3001fe92c0ff0c2fffc3c196ec
Grazielle Amanda do Carmo Café <grazielle.cafe@dtidigital.com.br>
2021-09-21 18:38:34 -0300

    Conta Bancária - arquivo executável em JavaScript

---
+++ js-dti-poo-classes/intro/ContaBancaria.js
@@ -0,0 +1,7 @@
+var ContaBancaria = /** @class */ (function () {
+    function ContaBancaria() {
+    }
+    return ContaBancaria;
+}());
+var contabancaria = new ContaBancaria();
+console.log(contabancaria);
